2bc564e8d50feb110d73d36804138bac8925c88d
mykangelo <mmykangelo@gmail.com>
2024-01-22 19:38:33 +0800

    Answer for exercise number 8

---
+++ exercise8.py
@@ -0,0 +1,18 @@
+
+
+
+
+#set a initial number for a rows in the patterns
+num = 5
+
+#create a first or outer loop 
+for i in range(1, num + 1):
+    #start a nested loop to achieve the pattern
+    for j in range(1, i + 1):
+        print(i, end=" ") # display the pattern with the iteration of nested loop
+
+    print("") #print a blank for a new line after each row
+
+
+
+
